09403bc501b04d160cd7e35b7b8b63f606cd1edf
MyNameJaeff <fredrik.andersson@chasacademy.se>
2025-02-08 16:08:38 +0100

    Update DOM_Handler.js

--- js/DOM_Handler.js
+++ js/DOM_Handler.js
@@ -1,7 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const header = document.getElementById("header");
-    header.appendChild(createNavbar());
-
     const easterEgg = document.getElementById("easterEgg");
     easterEgg.addEventListener("click", () => {
         easterEgg2();
